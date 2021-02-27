7c61efc4009fb1083620036a62eb318d288b3392
Matheus <matheuscorocher@gmail.com>
2021-02-27 16:28:05 -0300

    initialize the project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/matheusfcorocher.png" alt="Matheus F.Corocher"/>
+        <div>
+            <strong>Matheus F Corocher</strong>
+            <p>
+                <img src="icons/level.svg" alt="level" />
+                Level {level}
+            </p>
+        </div>
+        </div>
+    )
+}
